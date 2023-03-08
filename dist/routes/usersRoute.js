9a0c67b32667bed4311f8cf6a5eb9c65dc114d51
Cristian <cristian.munoz.2020@upb.edu.co>
2023-03-07 21:17:23 -0500

    Implementación del login y el registro de usuarios

---
+++ dist/routes/usersRoute.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const usersController_1 = __importDefault(require("../controller/usersController"));
+class usersRoute {
+    constructor() {
+        this.router = (0, express_1.Router)();
+        this.controller = new usersController_1.default();
+        this.config();
+    }
+    config() {
+        this.router.post("/login", this.controller.login);
+        this.router.post("/register", this.controller.register);
+    }
+}
+exports.default = usersRoute;
