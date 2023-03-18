dcf2e634be7f759dc80b5402846a40810181e776
Cristian <cristian.munoz.2020@upb.edu.co>
2023-03-18 17:49:37 -0500

    Correción del script de inicialización de la base de datos

--- infraestructura/database/main.sh
+++ infraestructura/database/main.sh
@@ -4,8 +4,8 @@
 
 mongosh <<EOF
 use admin
-db.createUser({user: ${mongo_root}, pwd: ${mongo_root_password}, roles: [ { >
+db.createUser( { user: ${mongo_root}, pwd: ${mongo_root_password}, roles: [ { role: "root", db: "admin" }, { role: "readWriteAnyDatabase", db: "admin" } ] } )
 use mienvio
-db.createUser({user: ${mongo_backend}, pwd: ${mongo_backend_password}, roles>
+db.createUser( { user: ${mongo_backend}, pwd: ${mongo_backend_password}, roles: [ { role: "readWrite", db: "mydb" } ] } )
 exit
 EOF
