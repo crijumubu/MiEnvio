0a10e277dd221b23a9436cc4a3d4674ffb86abd0
Cristian <cristian.munoz.2020@upb.edu.co>
2023-03-18 17:50:25 -0500

    Configuración de logs de la base de datos

--- infraestructura/database/main.sh
+++ infraestructura/database/main.sh
@@ -5,7 +5,9 @@
 mongosh <<EOF
 use admin
 db.createUser( { user: ${mongo_root}, pwd: ${mongo_root_password}, roles: [ { role: "root", db: "admin" }, { role: "readWriteAnyDatabase", db: "admin" } ] } )
+db.setProfilingLevel(2)
 use mienvio
 db.createUser( { user: ${mongo_backend}, pwd: ${mongo_backend_password}, roles: [ { role: "readWrite", db: "mydb" } ] } )
+db.setProfilingLevel(2)
 exit
 EOF
