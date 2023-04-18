19021de586df7ce017d4b0a012a78864fc8d1175
sdbaronc <47455237+sdbaronc@users.noreply.github.com>
2023-04-18 15:09:23 -0500

    creacion de interfaz para costos fijos

---
+++ backend/src/database/interface/IConfiguracion.ts
@@ -0,0 +1,8 @@
+import { Document } from 'mongoose';
+
+export default interface IConfiguracion extends Document {
+    idConfiguracion: number;
+    idVehiculoConfiguracion: number;
+    idCargaConfiguracion: number;
+    idUnidadConfiguracion: number; 
+}
