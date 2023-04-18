05ba82c592886dfbc421a4738e3822a5a5fe3372
sdbaronc <47455237+sdbaronc@users.noreply.github.com>
2023-04-18 15:05:31 -0500

    creacion de interfaz para promedios de velocidad

---
+++ backend/src/database/interface/IPromedioVelocidad.ts
@@ -0,0 +1,11 @@
+import { Document } from 'mongoose';
+
+export default interface IPromedioVelocidad extends Document {
+    idVelocidad: number;
+    idVehiculoVelocidad: number;
+    plano: number;
+    ondulado: number; 
+    montana: number;
+    urbano: number;
+    despavimentado: number;
+}
