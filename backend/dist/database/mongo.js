6a1430505d3d3dc5cbcae4dae93a982782a7df95
sdbaronc <47455237+sdbaronc@users.noreply.github.com>
2023-04-19 12:53:07 -0500

    adicion de schemas al modelo de mongo

--- backend/dist/database/mongo.js
+++ backend/dist/database/mongo.js
@@ -5,9 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const userSchema_1 = __importDefault(require("./schema/userSchema"));
+const ciudadSchema_1 = __importDefault(require("./schema/ciudadSchema"));
+const configuracionSchema_1 = __importDefault(require("./schema/configuracionSchema"));
+const costoFijoSchema_1 = __importDefault(require("./schema/costoFijoSchema"));
+const costoVariableSchema_1 = __importDefault(require("./schema/costoVariableSchema"));
+const peajeSchema_1 = __importDefault(require("./schema/peajeSchema"));
+const promedioConsumoSchema_1 = __importDefault(require("./schema/promedioConsumoSchema"));
+const promedioVelocidadSchema_1 = __importDefault(require("./schema/promedioVelocidadSchema"));
+const rutaPeajeSchema_1 = __importDefault(require("./schema/rutaPeajeSchema"));
+const rutaSchema_1 = __importDefault(require("./schema/rutaSchema"));
+const tipoCargaSchema_1 = __importDefault(require("./schema/tipoCargaSchema"));
+const tipoParametroCostoVariableSchema_1 = __importDefault(require("./schema/tipoParametroCostoVariableSchema"));
+const tipoUnidadSchema_1 = __importDefault(require("./schema/tipoUnidadSchema"));
+const tipoVehiculoSchema_1 = __importDefault(require("./schema/tipoVehiculoSchema"));
 class mongo {
     constructor(indexSchema) {
-        this.schemasList = [userSchema_1.default];
+        this.schemasList = [userSchema_1.default, ciudadSchema_1.default, configuracionSchema_1.default, costoFijoSchema_1.default, costoVariableSchema_1.default, peajeSchema_1.default, promedioConsumoSchema_1.default, promedioVelocidadSchema_1.default, rutaPeajeSchema_1.default, rutaSchema_1.default, tipoCargaSchema_1.default, tipoParametroCostoVariableSchema_1.default, tipoUnidadSchema_1.default, tipoVehiculoSchema_1.default];
         this.connect = () => {
             mongoose_1.default.set('strictQuery', true);
             mongoose_1.default.connect(this.uri)
--- backend/src/database/mongo.ts
+++ backend/src/database/mongo.ts
@@ -1,11 +1,23 @@
 import mongoose from 'mongoose';
 import userSchema from './schema/userSchema';
-
+import ciudadSchema from './schema/ciudadSchema';
+import configuracionSchema from './schema/configuracionSchema';
+import costoFijoSchema from './schema/costoFijoSchema';
+import costoVariableSchema from './schema/costoVariableSchema';
+import peajeSchema from './schema/peajeSchema';
+import promedioConsumoSchema from './schema/promedioConsumoSchema';
+import promedioVelocidadSchema from './schema/promedioVelocidadSchema';
+import rutaPeajeSchema from './schema/rutaPeajeSchema';
+import rutaSchema from './schema/rutaSchema';
+import tipoCargaSchema from './schema/tipoCargaSchema';
+import tipoParametroCostoVariableSchema from './schema/tipoParametroCostoVariableSchema';
+import tipoUnidadSchema from './schema/tipoUnidadSchema';
+import tipoVehiculoSchema from './schema/tipoVehiculoSchema';
 class mongo{
 
   private uri: string;
   public model: any;
-  public schemasList: any[] =[userSchema];
+  public schemasList: any[] =[userSchema,ciudadSchema,configuracionSchema,costoFijoSchema,costoVariableSchema,peajeSchema,promedioConsumoSchema,promedioVelocidadSchema,rutaPeajeSchema,rutaSchema,tipoCargaSchema,tipoParametroCostoVariableSchema,tipoUnidadSchema,tipoVehiculoSchema];
 
   constructor(indexSchema:number){
     
