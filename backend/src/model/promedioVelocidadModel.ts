c5d26b8564f30fafa61fb9c372a79bf079b09d3e
sdbaronc <47455237+sdbaronc@users.noreply.github.com>
2023-04-22 16:45:29 -0500

    creacion del modelo de velocidad

--- backend/src/model/promedioVelocidadModel.ts
+++ backend/src/model/promedioVelocidadModel.ts
@@ -7,6 +7,16 @@ class promedioVelocidadModel {
       this.mongo = new mongo(7);
       
     }
+    public obtenerVelocidades= async (fn:Function)=>{
+      this.mongo.connect();
+      const rows=await this.mongo.model.find();
+      fn(rows);
+  }
+  public idVelocidad= async (id:number, fn:Function)=>{
+      this.mongo.connect();
+      const rows=await this.mongo.model.find({idVelocidad:id});
+      fn(rows);
+  }
   
 }
 export default promedioVelocidadModel;
