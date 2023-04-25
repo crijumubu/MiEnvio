b9655434283ee8f2a68cfaa1b538cc985eb52e93
sdbaronc <47455237+sdbaronc@users.noreply.github.com>
2023-04-25 16:46:11 -0500

    add endpoint costo variable

--- backend/src/model/costoFijoModel.ts
+++ backend/src/model/costoFijoModel.ts
@@ -7,6 +7,11 @@ class costoFijoModel {
       this.mongo = new mongo(3);
       
     }
+    public obtenerCostosFijos= async (id:number, fn:any)=>{
+      this.mongo.connect();
+      const rows=await this.mongo.model.find({idConfiguracionVehiculo:id});
+      fn(rows);
+  }
   
 }
 export default costoFijoModel;
