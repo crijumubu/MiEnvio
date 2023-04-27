a7e84caa55e242778feea51bd8720b811cc99b14
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2023-04-27 12:55:33 -0500

    Vista generar QR

--- frontend/lib/views/generate_qr/generate_qr.dart
+++ frontend/lib/views/generate_qr/generate_qr.dart
@@ -1,18 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import '../register/widgets/header_back.dart';
 
-class GenerateQR extends StatelessWidget {
-  const GenerateQR({super.key});
+class GenerateQR extends StatefulWidget {
+  const GenerateQR({super.key, required this.hasAppbar, required this.title, required this.qrData});
+  final bool hasAppbar;
+  final String title;
+  final double qrData;
+
+  @override
+  State<GenerateQR> createState() => _GenerateQRState();
+}
+
+class _GenerateQRState extends State<GenerateQR> {
+  Widget? qr;
+
+   
+  void generar(String p) {
+    setState(() {
+      qr = QrImage(
+        data: widget.qrData.toString(),
+        version: QrVersions.auto,
+        size: 200.0,
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: headerBack(context),
+    // if(hasAppbar is bool) final 
+    return (widget.hasAppbar) ? Scaffold(
+      appBar: headerBack(context) ,
       body: Center(
-        child: Text("Generar QR", style: GoogleFonts.rubik(fontSize: 64),),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Center(child: Text(widget.title, style: GoogleFonts.rubik(fontSize: 34 ,fontWeight: FontWeight.bold),))
+            ),
+            Expanded(
+              flex: 8,
+              child: Column(
+                children: [
+                  QrImage(
+                    data: widget.qrData.toString(),
+                    version: QrVersions.auto,
+                    size: 300.0,
+                  ),
+                  const SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Text("Muestre este QR a un supervisor, para que se le asigne un viaje.", style: GoogleFonts.rubik(fontSize: 22), textAlign: TextAlign.justify,),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       )
-    );
+    ) : 
+    
+    // ? Vista supervisor
+    Center(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.red,
+                child: Text(widget.title, style: GoogleFonts.rubik(fontSize: 32),)
+              )
+            ),
+            Expanded(
+              flex: 8,
+              child: Column(
+                children: [
+                  QrImage(
+                    data: widget.qrData.toString(),
+                    version: QrVersions.auto,
+                    size: 300.0,
+                  ),
+                  Text("Muestre este QR a un supervisor, para que se le asigne un viaje", style: GoogleFonts.rubik(fontSize: 16),)
+                ],
+              ),
+            )
+          ],
+        ),
+      );
   }
 }
--- frontend/lib/views/scan_qr/scan_qr.dart
+++ frontend/lib/views/scan_qr/scan_qr.dart
@@ -1,18 +1,96 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
-import '../register/widgets/header_back.dart';
+class ScanQR extends StatefulWidget {
+  ScanQR({super.key});
 
-class ScanQR extends StatelessWidget {
-  const ScanQR({super.key});
+  @override
+  State<ScanQR> createState() => _ScanQRState();
+}
+
+class _ScanQRState extends State<ScanQR> {
+  // late double width;
+
+  MobileScannerController cameraController = MobileScannerController(
+
+  );
 
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: headerBack(context),
-      body: Center(
-        child: Text("Escanear QR", style: GoogleFonts.rubik(fontSize: 64),),
-      )
+        appBar: AppBar(
+          title: const Text('Mobile Scanner'),
+          actions: [
+            IconButton(
+              color: Colors.white,
+              icon: ValueListenableBuilder(
+                valueListenable: cameraController.torchState,
+                builder: (context, state, child) {
+                  switch (state as TorchState) {
+                    case TorchState.off:
+                      return const Icon(Icons.flash_off, color: Colors.grey);
+                    case TorchState.on:
+                      return const Icon(Icons.flash_on, color: Colors.yellow);
+                  }
+                },
+              ),
+              iconSize: 32.0,
+              onPressed: () => cameraController.toggleTorch(),
+            ),
+            IconButton(
+              color: Colors.white,
+              icon: ValueListenableBuilder(
+                valueListenable: cameraController.cameraFacingState,
+                builder: (context, state, child) {
+                  switch (state as CameraFacing) {
+                    case CameraFacing.front:
+                      return const Icon(Icons.camera_front);
+                    case CameraFacing.back:
+                      return const Icon(Icons.camera_rear);
+                  }
+                },
+              ),
+              iconSize: 32.0,
+              onPressed: () => cameraController.switchCamera(),
+            ),
+          ],
+        ),
+        body: Stack(
+          children: [
+            MobileScanner(
+              startDelay: true,
+              // fit: BoxFit.contain,
+              controller: cameraController,
+              onDetect: (capture) {
+                final List<Barcode> barcodes = capture.barcodes;
+                final Uint8List? image = capture.image;
+                for (final barcode in barcodes) {
+                  debugPrint('Barcode found! ${barcode.rawValue}');
+                }
+              },
+              // scanWindow: ,
+            ),
+            GestureDetector(
+              onTap: (){
+                      
+              }, 
+              child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  // border: (),
+                  shape: BoxShape.circle,
+                  color: Colors.white
+                ),
+                child: Icon(Icons.flash_off, size: 30,)
+              )
+            ),
+            // SizedBox(height: 10,)
+          ],
+        ),
     );
   }
 }
