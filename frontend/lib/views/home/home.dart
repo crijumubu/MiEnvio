53bb3f358ceabbd4355e30cda5d7c6d791a1ee86
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2023-05-06 10:34:22 -0500

    :zzz: Actualizacion home conductor

--- frontend/lib/views/home/home.dart
+++ frontend/lib/views/home/home.dart
@@ -46,78 +46,75 @@ class _HomeState extends State<Home> {
       ),
       body: WillPopScope(
         onWillPop: () async => false ,
-        child: Center(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 20),
-                    height: MediaQuery.of(context).size.height*0.25,
-                    width: double.infinity,
-                    decoration: const BoxDecoration(
-                      color: Color(0xff344E41),
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(50),
-                        bottomRight: Radius.circular(50),
-                      )
-                    ),
-                    child:  Center(
-                      child: Row(
-                        children: [
-                          const SizedBox(width: 30,),
-                          RichText(
-                            text:  TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: "Bienvenido\n",
-                                  style: GoogleFonts.rubik(
-                                    fontSize: 24,
-                                  )
-
-                                ),
-                                TextSpan(
-                                  text: "${usuario.name}",
-                                  style: GoogleFonts.rubik(
-                                    fontSize: 32,
-                                    // fontStyle: FontStyle.italic,
-                                    fontWeight: FontWeight.w600,
-                                  )
-                                )
-                              ]
-                            ),
-                          ),
-                          const Spacer(),
-                          TextButton(
-                            onPressed: (){
-                              Navigator.pushNamed(context, "initial-page");
-                            }, 
-                            child: Image.asset("assets/images/logout.png", scale: 17, color: const Color(0xffffffff),)
-                          ),
-                          const SizedBox(width: 5,),
-                        ],
-                      )
-                    ),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  height: MediaQuery.of(context).size.height*0.25,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Color(0xff344E41),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    )
                   ),
+                  child:  Center(
+                    child: Row(
+                      children: [
+                        const SizedBox(width: 30,),
+                        RichText(
+                          text:  TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "Bienvenido\n",
+                                style: GoogleFonts.rubik(
+                                  fontSize: 24,
+                                )
 
+                              ),
+                              TextSpan(
+                                text: "${usuario.name}",
+                                style: GoogleFonts.rubik(
+                                  fontSize: 32,
+                                  // fontStyle: FontStyle.italic,
+                                  fontWeight: FontWeight.w600,
+                                )
+                              )
+                            ]
+                          ),
+                        ),
+                        const Spacer(),
+                        TextButton(
+                          onPressed: (){
+                            Navigator.pushNamed(context, "initial-page");
+                          }, 
+                          child: Image.asset("assets/images/logout.png", scale: 17, color: const Color(0xffffffff),)
+                        ),
+                        const SizedBox(width: 5,),
+                      ],
+                    )
+                  ),
+                ),
+
+              ],
+            ),
+            const SizedBox(height: 50,),
+            Center(
+              child: Wrap(
+                children:  [
+                  MenuButton(text: 'Viajes', imgRoute: 'delivery2.png', btnRoute: '/shippings-driver', args: widget.id ,),
+                  MenuButton(text: 'Escanear QR', imgRoute: 'camara.png', btnRoute: '/scan-qr',),
+                  MenuButton(text: 'Generar QR', imgRoute: 'codigo-qr.png', btnRoute: '/generate-qr', args: QrGenerator(true, "QR Conductor", usuario.id),) ,
+                  MenuButton(text: 'Perfil', imgRoute: 'user.png', btnRoute: '/user', args: UserView(hasAppBar: true, name: usuario.name, email: usuario.email, password: usuario.password, id:usuario.id, updateUser: getUser),),
                 ],
               ),
-              const SizedBox(height: 50,),
-              Center(
-                child: Wrap(
-                  children:  [
-                    MenuButton(text: 'Viajes', imgRoute: 'delivery2.png', btnRoute: '/shippings-driver', args: widget.id ,),
-                    MenuButton(text: 'Escanear QR', imgRoute: 'camara.png', btnRoute: '/scan-qr',),
-                    MenuButton(text: 'Generar QR', imgRoute: 'codigo-qr.png', btnRoute: '/generate-qr', args: QrGenerator(true, "QR Conductor", usuario.id),) ,
-                    MenuButton(text: 'Perfil', imgRoute: 'user.png', btnRoute: '/user', args: UserView(hasAppBar: true, name: usuario.name, email: usuario.email, password: usuario.password, id:usuario.id, updateUser: getUser),),
-                  ],
-                ),
-              )
+            )
 
         
-            ],
-          ),
+          ],
         ),
       ),
     );
