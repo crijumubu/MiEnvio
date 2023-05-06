9f8ffcac5aa172e64b7c8cf46d7616921f686436
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2023-05-06 10:36:43 -0500

    :suspect: : Geocodig service api google

--- frontend/lib/controller/auth_controller.dart
+++ frontend/lib/controller/auth_controller.dart
@@ -123,6 +123,19 @@ class AuthController{
     return null;
   }
 
+  static Future<String?> addressToLatLn(Uri uri)async{
+    try {
+      final response = await http.get(uri);
+      if(response.statusCode == 200){
+        return response.body;
+      } 
+  
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+    return null;
+  }
+
   Future setDriver(context, int idViaje, int idConductor)async{
     final body = '{"idViaje": ${idViaje},"idConductor":${idConductor}}';
     final headers = {"Content-type":'application/json'};
